b4b3f57aba26fb7f39dfa8e742e23419ae30d2ea
J531-wq <omagujoseph95@gmail.com>
2025-12-02 12:35:48 +0100

    Made some change in requirements.txt for the fourth time

--- social/asgi.py
+++ social/asgi.py
@@ -8,14 +8,27 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-import core.routing
+import django # <-- ADD THIS LINE
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social.settings")
 
+# ----------------------------------------------------
+# 1. Initialize Django App Registry
+# This must happen BEFORE importing any code (like routing) that uses models.
+django.setup() # <-- ADD THIS LINE
+# ----------------------------------------------------
+
+# 2. Import Channel and app-dependent components AFTER setup()
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import core.routing # <-- MOVE THIS IMPORT DOWN HERE
+
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(URLRouter(core.routing.websocket_urlpatterns)),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            core.routing.websocket_urlpatterns
+        )
+    ),
 })
